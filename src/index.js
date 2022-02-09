ea9239937e868908e73a10c99c04831f3d80c571
Ivan <aguirreivan96@gmail.com>
2022-02-08 19:52:49 -0600

    Added Luigi headshot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css';
+import Headshot from './images/Luigi-Head-removebg-preview.png';
 
-console.log("This works");
+//add headshot to page
+let headshotImg = document.createElement("img");
+headshotImg.src = Headshot;
+let contentDiv = document.getElementById("content");
+contentDiv.appendChild(headshotImg);
+
