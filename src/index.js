0d238e5824076de05705cb44db54065ffcf5849d
Ivan <aguirreivan96@gmail.com>
2022-03-21 22:39:52 -0500

    Added textContent box using JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './style.css';
-import Headshot from './images/Luigi-Head-removebg-preview.png';
+// import Headshot from './images/Luigi-Head-removebg-preview.png';
 
-//add headshot to page
-let headshotImg = document.createElement("img");
-headshotImg.src = Headshot;
+// getting contentDiv from html
 let contentDiv = document.getElementById("content");
-contentDiv.appendChild(headshotImg);
+
+//creating a text content div to store text box
+let textContent = document.createElement("div");
+textContent.classList.add("textContent");
+contentDiv.appendChild(textContent);
+
+// //add headshot to page
+// // let headshotImg = document.createElement("img");
+// // headshotImg.src = Headshot;
+// // let contentDiv = document.getElementById("content");
+// // contentDiv.appendChild(headshotImg);
+// console.log("This works");
 
