85cc924b85820850c92721e0c69b8bf601522171
Ivan <aguirreivan96@gmail.com>
2022-03-21 22:49:32 -0500

    Added landing header and paragraph with styling and centered

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ let textContent = document.createElement("div");
 textContent.classList.add("textContent");
 contentDiv.appendChild(textContent);
 
+//adding headline and landing paragraph to text box
+let headline = document.createElement("div");
+headline.classList.add("headline");
+headline.textContent = "Luigi's Pizzera";
+let landingPara = document.createElement("div");
+landingPara.classList.add("paragraph");
+landingPara.textContent = "After a hard day of helping Mario rescue Princess Peach from his nemesis Bowser, Luigi likes to come home to a nice hot pizza. However, after a couple of bad pies, Luigi felt that nobody should come home to a disappointing pizza. So Luigi decided to show the world how a pizza should be made."
+textContent.appendChild(headline);
+textContent.appendChild(landingPara);
+
 // //add headshot to page
 // // let headshotImg = document.createElement("img");
 // // headshotImg.src = Headshot;
