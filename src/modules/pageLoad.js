add2a85c7120a15eea2fd18d1767a81118033dc3
Ivan <aguirreivan96@gmail.com>
2022-04-18 15:55:21 -0500

    Prepping src folder for module structure

---
+++ src/modules/pageLoad.js
@@ -0,0 +1,8 @@
+console.log("Test");
+
+//get content div item
+let contentDiv = document.getElementById("content");
+
+//adding headerDiv to content section
+let headerDiv = document.createElement("div");
+contentDiv.appendChild(headerDiv);
