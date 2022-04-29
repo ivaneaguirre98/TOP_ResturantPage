d95acfe1235b321ce5e58febf96e1310a0b4ba7d
Ivan <aguirreivan96@gmail.com>
2022-04-29 15:19:21 -0500

    Built out menu page and connected it to index.js

--- src/modules/menuPage.js
+++ src/modules/menuPage.js
@@ -10,12 +10,40 @@ function init(){
 function createMenuPage(){
     console.log("This came from MenuPage.js");
 
+    //create innerContent Div for page content
     let innerContentDiv = document.createElement("div");
     innerContentDiv.classList.add("innerContent");
     mainSection.appendChild(innerContentDiv);
 
-
+//get innerContent div assigned to variable
     let innerContent = document.querySelector(".innerContent");
+
+    //menu title
+    let menuTitle = document.createElement("h2");
+    menuTitle.innerHTML = "Menu";
+    menuTitle.classList.add("menuTitle");
+    innerContent.appendChild(menuTitle);
+
+    //createMenu
+    let menuItem1 = document.createElement("div");
+    menuItem1.innerHTML = "Super Pizza ....... $6.99"
+    menuItem1.classList.add("menuItem");
+    innerContent.appendChild(menuItem1);
+
+    let menuItem2 = document.createElement("div");
+    menuItem2.innerHTML = "Super Calzone ....... $6.99"
+    menuItem2.classList.add("menuItem");
+    innerContent.appendChild(menuItem2);
+
+    let menuItem3 = document.createElement("div");
+    menuItem3.innerHTML = "Super Bread ....... $3.99"
+    menuItem3.classList.add("menuItem");
+    innerContent.appendChild(menuItem3);
+
+    let menuItem4 = document.createElement("div");
+    menuItem4.innerHTML = "Super Wings ....... $9.99"
+    menuItem4.classList.add("menuItem");
+    innerContent.appendChild(menuItem4);
 }
 
 function menuPage(){
